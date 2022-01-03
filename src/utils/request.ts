71e2e3df707fcf49532d5d4d3a09c4bca6217957
yuchiXiong <yuchi.xiong@foxmail.com>
2022-01-03 20:02:04 +0800

    feat: 完成基本请求单例的封装；

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,37 +1,83 @@
 import Taro from "@tarojs/taro";
 
+interface IRequestParams {
+  url: string,
+  method: 'OPTIONS' | 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'TRACE' | 'CONNECT',
+  data?: Object
+}
+
+export interface IStandardResponse<T> {
+  data?: T
+  errorMessage?: string
+}
+
+interface ILoginResponse {
+  data: {
+    jwt: string
+  }
+}
+
+/**
+ * @description 自动通过微信登录并换取服务端jwt
+ */
+const login = () => new Promise((resolve, reject) => {
+  Taro.login({
+    success: res => {
+      Taro.request({
+        url: 'http://localhost:3000/user/sessions',
+        method: 'POST',
+        data: {
+          code: res.code
+        },
+        success: (res) => {
+          const response = res.data as ILoginResponse;
+          Taro.setStorageSync('harvest:jwt', response.data.jwt);
+          resolve(response.data.jwt);
+        },
+        fail: reject
+      })
+    }
+  });
+})
+
+/**
+ * @description 请求前置
+ * 1. 如果请求时无token，自动拉取token并携带token
+ */
 Taro.addInterceptor(function (chain) {
   const requestParams = chain.requestParams
-  const { method, data, url } = requestParams
-
-  console.log(`http ${method || 'GET'} --> ${url} data: `, data)
-
-  if (!Taro.getStorageSync('harvest:jwt') && !(url === 'http://localhost:3000/user/sessions')) {
-    Taro.login({
-      success: res => {
-        Taro.request({
-          url: 'http://localhost:3000/user/sessions',
-          method: 'POST',
-          data: {
-            code: res.code
-          },
-          success: (res) => {
-            Taro.setStorageSync('harvest:jwt', res.data.data.jwt);
-          }
-        })
-      }
-    });
+  requestParams.header = {
+    ...requestParams.header,
+    'access-token': Taro.getStorageSync('harvest:jwt')
   }
 
   return chain.proceed(requestParams)
-    .then(res => {
-      console.log(`http <-- ${url} result:`, res)
-      return res
-    })
+    .then(async res => {
+      if (res.statusCode === 401) {
+        await login();
+        requestParams.header = {
+          ...requestParams.header,
+          'access-token': Taro.getStorageSync('harvest:jwt')
+        }
+        return chain.proceed(requestParams);
+      } else {
+        return res.data;
+      }
+    });
 })
 
-const httpServer = (options) => {
-  return Taro.request(options);
+const request: (params: IRequestParams) => Promise<IStandardResponse<any>> = params => {
+  return new Promise((resolve, reject) => {
+    Taro.request({
+      url: params.url,
+      method: params.method,
+      data: params.data,
+      success: (res) => {
+        resolve(res.data);
+      },
+      fail: reject
+    })
+  });
 }
 
-export default httpServer;
+export default request;
