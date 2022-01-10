5e8f3ce4e4742faa330f2bffc1dba992ffbec8a5
yuchiXiong <yuchi.xiong@foxmail.com>
2022-01-10 22:53:55 +0800

    feat: 根据记录的具体日期对数据进行分组展示，在分组信息的右侧展示当日的总支出；

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,26 +1,28 @@
 import { useState, useEffect } from 'react';
-import Taro from '@tarojs/taro';
-import { ScrollView } from '@tarojs/components';
+import Taro, { usePullDownRefresh, useReachBottom } from '@tarojs/taro';
 import { AtCard, AtList, AtListItem } from 'taro-ui';
 import { CalendarThirtyTwo } from '@icon-park/react';
 import dayjs from 'dayjs';
+import 'dayjs/locale/zh-cn';
 import { IBill } from '../../api/bill';
 import API from '../../api';
 import styles from './index.module.scss';
 
+dayjs.locale('zh-cn');
+
 const IndexPage = () => {
 
   const [bills, setBills] = useState<IBill[]>([]);
-  const [month, setMonth] = useState<string>(dayjs().format('YYYY年M月'));
+  // const [month, setMonth] = useState<string>(dayjs().format('YYYY年M月'));
 
   useEffect(() => {
     fetchMore();
   }, []);
 
   const fetchMore = () => {
-    API.getBills(dayjs().format('YYYY-MM-DD')).then(res => {
+    API.getBills(dayjs().subtract(1, 'month').format('YYYY-MM-DD')).then(res => {
       setBills([...bills.concat(res.bills)]);
-      setMonth(dayjs(res.recordedAtByMonth).format('YYYY年M月'));
+      // setMonth(dayjs(res.recordedAtByMonth).format('YYYY年M月'));
       Taro.setNavigationBarTitle({
         title: dayjs(res.recordedAtByMonth).format('YYYY年M月')
       })
@@ -33,42 +35,58 @@ const IndexPage = () => {
     });
   }
 
-  const scrollStyle = {
-    height: '100vh'
+  // usePullDownRefresh(() => {
+  //   console.log('pull')
+  // });
+
+  // useReachBottom(() => {
+  //   console.log('bottom')
+  // });
+
+  /**
+   * @description 是否应该展示记录日期的分组标题
+   * @param {IBill} item - 当前项 
+   * @param {Number} index - 当前项下标
+   * @returns {Boolean} 是否应该显示 title
+   */
+  const shouldIShowTitle: (item: IBill, index: number) => boolean = (item, index) => {
+    if (index === 0) { // 如果是第一项则直接返回true
+      return true;
+    }
+    const currentDay = dayjs(item.recordedAt).format('YYYY/MM/DD');
+    const preDay = dayjs(bills[index - 1].recordedAt).format('YYYY/MM/DD');
+    return currentDay !== preDay;
   }
 
-  return <ScrollView
-    onScrollToLower={fetchMore}
-    onScrollToUpper={() => { }}
-    scrollY
-    scrollTop={0}
-    style={scrollStyle}
-    refresherThreshold={100}
-  >
+  const totalAmount: (current: IBill) => string = current => {
+    const billGroupByDay = bills.filter(bill => dayjs(bill.recordedAt).format('YYYY/MM/DD') === dayjs(current.recordedAt).format('YYYY/MM/DD'))
+    return billGroupByDay.reduce((result, item) => result += Number(item.amount) * (item.inOrOut === 1 ? -1 : 1), 0).toFixed(2);
+  }
 
-    <AtList>
-      {
-        bills.map(item => <>
-          <AtCard
-            title={month}
+  return <AtList>
+    {
+      bills.map((item, index) => <>
+        {
+          shouldIShowTitle(item, index) && <AtCard
+            title={dayjs(item.recordedAt).format('YYYY/MM/DD dddd')}
             renderIcon={
               <CalendarThirtyTwo theme="multi-color" size="24" fill={['#333', '#ff8ca0', '#FFF', '#43CCF8']} />
             }
+            extra={totalAmount(item)}
             isFull={true}
             className={styles.title}
           />
-          <AtListItem key={item.id}
-            title={item.description}
-            note={dayjs(item.recordedAt).format('YYYY-MM-DD')}
-            extraText={`${item.inOrOut === 1 ? '-' : '+'} ${Number(item.amount).toFixed(2)}`}
-            arrow={null}
-            thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
-          />
-        </>)
-      }
-    </AtList>
+        }
+        <AtListItem key={item.id}
+          title={item.description}
+          extraText={`${item.inOrOut === 1 ? '-' : '+'} ${Number(item.amount).toFixed(2)}`}
+          arrow={null}
+          thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+        />
+      </>)
+    }
+  </AtList>
 
-  </ScrollView>
 };
 
 export default IndexPage;
---
+++ src/pages/mine/index.tsx
@@ -0,0 +1,11 @@
+import Taro, { usePullDownRefresh, useReachBottom } from '@tarojs/taro';
+import { View } from '@tarojs/components';
+
+const MinePage = () => {
+
+
+  return <View>我的</View>
+
+};
+
+export default MinePage;
